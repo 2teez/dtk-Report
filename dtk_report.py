f080204c502bd0b14fbf41d2e79875b9df9d55b6
timothy adigun <adigun.timothy@gmail.com>
2022-09-26 09:38:50 +0100

    modified dtk and dtk_report py files

--- dtk_report.py
+++ dtk_report.py
@@ -1,11 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import os
 import sys
 
 
 def main(lst: list[str]) -> None:
-    ...
+    try:
+        hour = int(lst[0])
+        if (hour < 0 or hour > 23):
+            print('Enter a valid report time between 0 & 23')
+            sys.exit(1)
+    except Exception as ex:
+        print(f'Usage: {sys.argv[0]} [timer]')
+        sys.exit(1)
 
 
 if __name__ == '__main__':
